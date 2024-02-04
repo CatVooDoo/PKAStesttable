074b7e8503add4d5b07b5aff8de77e7a56ceb75d
CatVooDoo <96387518+CatVooDoo@users.noreply.github.com>
2024-02-04 14:10:10 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -2,9 +2,9 @@ const profCard = document.querySelector(".proff--card");
 
 profCard.addEventListener('touchstart', function () {
     profCard.classList.add('proff--card-hover');
-    window.location.href = "https://pkas58.ru/article/070201"
 });
 
 profCard.addEventListener('touchend', function () {
     profCard.classList.remove('proff--card-hover');
+    window.location.href = "https://pkas58.ru/article/070201"
 });
