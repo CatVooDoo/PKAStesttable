15fccb74cc5bd6b6ab33be629425a3f74ba12fb3
CatVooDoo <96387518+CatVooDoo@users.noreply.github.com>
2024-02-05 20:32:52 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,34 +1,7 @@
-// const profCard = document.querySelector(".architecture");
+const profCard = document.querySelectorAll(".proff--card");
 
-// profCard.addEventListener('touchstart', function () {
-//     profCard.classList.add('proff--card-hover');
-// });
-
-// profCard.addEventListener('touchend', function () {
-//     profCard.classList.remove('proff--card-hover');
-//     // window.location.href = "https://pkas58.ru/article/070201"
-// });
-
-// one two three four five
-
-const profCard1 = document.getElementById("one");
-const profCard2 = document.getElementById("two");
-const profCard3 = document.getElementById("three");
-const profCard4 = document.getElementById("four");
-const profCard5 = document.getElementById("five");
-
-
-window.onload = function() {
+profCard.forEach(function(speciallity) {
     setTimeout(function() {
-        profCard1.classList.add('proff-card-anim')
+        speciallity.classList.add('proff-card-anim');
     }, 100);
-    setTimeout(function() {
-        profCard2.classList.add('proff-card-anim')
-    }, 200);
-    setTimeout(function() {
-        profCard3.classList.add('proff-card-anim')
-    }, 300);
-    setTimeout(function() {
-        profCard4.classList.add('proff-card-anim')
-    }, 400);
-}
+});
