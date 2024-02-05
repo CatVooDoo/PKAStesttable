45e6b3fd387256c93f35ce109d8589d526a0aa8f
CatVooDoo <96387518+CatVooDoo@users.noreply.github.com>
2024-02-05 20:46:46 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -4,4 +4,10 @@ profCard.forEach(function(speciallity) {
     setTimeout(function() {
         speciallity.classList.add('proff-card-anim');
     }, 100);
+    speciallity.addEventListener('touchstart', function(){
+        speciallity.classList.add('proff--card-active')
+    });
+    speciallity.addEventListener('touchend', function(){
+        speciallity.classList.remove('proff--card-active')
+    });
 });
