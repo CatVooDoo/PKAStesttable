4a6ae8d7ecfce9b5017999e59cb95daab32b4812
CatVooDoo <96387518+CatVooDoo@users.noreply.github.com>
2024-02-07 09:34:00 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,13 +1,21 @@
 const profCard = document.querySelectorAll(".proff--card");
+const cardMoreInfo = document.querySelectorAll(".more--info");
 
 profCard.forEach(function(speciallity) {
     setTimeout(function() {
         speciallity.classList.add('proff-card-anim');
     }, 100);
     speciallity.addEventListener('touchstart', function(){
-        speciallity.classList.add('proff--card-active')
+        speciallity.classList.add('proff--card-active');
+        cardMoreInfo.forEach(function(info) {
+            info.classList.add('more--info-active')
+        });
     });
     speciallity.addEventListener('touchend', function(){
-        speciallity.classList.remove('proff--card-active')
+        speciallity.classList.remove('proff--card-active');
+        cardMoreInfo.forEach(function(info) {
+            info.classList.remove("more--info-active")
+        });
     });
 });
+
