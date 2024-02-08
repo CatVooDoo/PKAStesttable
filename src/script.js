0ec512e6df9946552669074621f8ec1dfbab989f
CatVooDoo <96387518+CatVooDoo@users.noreply.github.com>
2024-02-08 08:25:03 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,33 +1,21 @@
 const profCard = document.querySelectorAll(".proff--card");
 const cardMoreInfo = document.querySelectorAll(".more--info");
 
-    profCard.forEach(function(speciallity) {
-        setTimeout(function() {
-            speciallity.classList.add('proff-card-anim');
-        }, 100);
-        // speciallity.addEventListener('touchstart', function(){
-        //     speciallity.classList.add('proff--card-active');
-        //     cardMoreInfo.forEach(function(info) {
-        //         info.classList.add('more--info-active')
-        //     });
-        // });
-        // speciallity.addEventListener('touchend', function(){
-        //     speciallity.classList.remove('proff--card-active');
-        //     cardMoreInfo.forEach(function(info) {
-        //         info.classList.remove("more--info-active")
-        //     });
-        // });
-    });
+profCard.forEach(function (speciallity) {
+    setTimeout(function () {
+        speciallity.classList.add('proff-card-anim');
+    }, 200);
+});
 
 
-profCard.forEach(function(speciallity) {
-    speciallity.addEventListener('touchstart', function(event) {
+profCard.forEach(function (speciallity) {
+    speciallity.addEventListener('touchstart', function (event) {
         event.stopPropagation();
         speciallity.classList.add('proff--card-active');
         const index = Array.from(profCard).indexOf(speciallity);
         cardMoreInfo[index].classList.add('more--info-active');
     });
-    speciallity.addEventListener('touchend', function() {
+    speciallity.addEventListener('touchend', function () {
         speciallity.classList.remove('proff--card-active');
         const index = Array.from(profCard).indexOf(speciallity);
         cardMoreInfo[index].classList.remove('more--info-active');
