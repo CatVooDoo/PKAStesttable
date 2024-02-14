ce557b4e752c26277bc7a0b57cfdf6a32ac61e85
CatVooDoo <96387518+CatVooDoo@users.noreply.github.com>
2024-02-14 09:38:45 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,13 +1,12 @@
 const profCard = document.querySelectorAll(".proff--card");
 const cardMoreInfo = document.querySelectorAll(".more--info");
 
-const proffIndex = ['https://pkas58.ru/article/070201', 'https://pkas58.ru/article/080201', 'https://pkas58.ru/article/090207',
-'https://pkas58.ru/article/540120']
-
+// URL адреса специальностей
+const proffIndex = ['https://pkas58.ru/article/070201', 'https://pkas58.ru/article/080201', 'https://pkas58.ru/article/090207', 'https://pkas58.ru/article/540120'];
 
 profCard.forEach(function (speciallity) {
     setTimeout(() => {
-        speciallity.classList.add('proff-card-anim')
+        speciallity.classList.add('proff-card-anim');
     }, 200);
 });
 
@@ -16,22 +15,30 @@ profCard.forEach(function (speciallity) {
         event.stopPropagation();
         if (speciallity.classList.contains('proff--card-active')) {
             const index = Array.from(profCard).indexOf(speciallity);
-            speciallity.classList.add('progg--card-touch');
-            window.location.assign(proffIndex[index]);  
-            
+            speciallity.classList.add('progg--card-touch'); 
+            window.location.assign(proffIndex[index]); // отправка на страницу пкаса
         } else {
+            // скрытие первой карточки
+            profCard.forEach(function (otherCard) {
+                if (otherCard !== speciallity) {
+                    otherCard.classList.remove('proff--card-active');
+                    const otherIndex = Array.from(profCard).indexOf(otherCard);
+                    cardMoreInfo[otherIndex].classList.remove('more--info-active');
+                }
+            });
+
             speciallity.classList.add('proff--card-active');
             const index = Array.from(profCard).indexOf(speciallity);
-            setTimeout(function() {
+            setTimeout(() => {
                 cardMoreInfo[index].classList.add('more--info-active');
             }, 1);
         }
     });
 });
 
-document.addEventListener('touchstart', function(event) {
+document.addEventListener('touchstart', function (event) {
     if (!event.target.closest('.proff--card-active')) {
-        profCard.forEach(function(speciallity) {
+        profCard.forEach(function (speciallity) {
             speciallity.classList.remove('proff--card-active');
             const index = Array.from(profCard).indexOf(speciallity);
             cardMoreInfo[index].classList.remove('more--info-active');
