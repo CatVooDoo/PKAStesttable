d0cba00110dacc62055b0dbd3cfa7eb513d0ab70
CatVooDoo <96387518+CatVooDoo@users.noreply.github.com>
2024-02-14 09:23:21 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,13 +1,32 @@
 const profCard = document.querySelectorAll(".proff--card");
 const cardMoreInfo = document.querySelectorAll(".more--info");
 
-const proffIndex = ['https://pkas58.ru/article/070201', 'https://pkas58.ru/article/080201', 'https://pkas58.ru/article/090207',]
+const proffIndex = ['https://pkas58.ru/article/070201', 'https://pkas58.ru/article/080201', 'https://pkas58.ru/article/090207',
+'https://pkas58.ru/article/540120']
 
 
 profCard.forEach(function (speciallity) {
-    setTimeout(function () {
-        speciallity.classList.add('proff-card-anim');
+    setTimeout(() => {
+        speciallity.classList.add('proff-card-anim')
     }, 200);
+});
+
+profCard.forEach(function (speciallity) {
+    speciallity.addEventListener('touchstart', function (event) {
+        event.stopPropagation();
+        if (speciallity.classList.contains('proff--card-active')) {
+            const index = Array.from(profCard).indexOf(speciallity);
+            speciallity.classList.add('progg--card-touch');
+            window.location.assign(proffIndex[index]);  
+            
+        } else {
+            speciallity.classList.add('proff--card-active');
+            const index = Array.from(profCard).indexOf(speciallity);
+            setTimeout(function() {
+                cardMoreInfo[index].classList.add('more--info-active');
+            }, 1);
+        }
+    });
 });
 
 document.addEventListener('touchstart', function(event) {
@@ -18,20 +37,4 @@ document.addEventListener('touchstart', function(event) {
             cardMoreInfo[index].classList.remove('more--info-active');
         });
     }
-});
-
-profCard.forEach(function (speciallity) {
-    speciallity.addEventListener('touchstart', function (event) {
-        event.stopPropagation();
-        if (speciallity.classList.contains('proff--card-active')) {
-            const index = Array.from(profCard).indexOf(speciallity);
-            window.location.href = proffIndex[index]; //Отсылка на страницу 
-        } else {
-            speciallity.classList.add('proff--card-active');
-            const index = Array.from(profCard).indexOf(speciallity);
-            setTimeout(function() {
-                cardMoreInfo[index].classList.add('more--info-active');
-            }, 1);
-        }
-    });
 });
